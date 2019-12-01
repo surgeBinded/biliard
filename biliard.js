82a753275ebeb750d2588c53cecae75ed48eb990
surgeBinded <ropotdaniel@gmail.com>
2019-12-01 20:52:29 +0100

    added shadows, did some cleanup

--- biliard.js
+++ biliard.js
@@ -1,24 +1,36 @@
-const FRONT_FEET_X = -5;
-const BACK_FEET_X = 25;
-const LEFT_FEET_Z = 0;
-const RIGHT_FEET_Z = 15;
-const TABLE_FEET_Y = 0;
-
-const TABLE_FEET_COLOR = "#663300";
-const LIGHT_COLOR = "#f5f57a";
-const TABLE_COLOR = "#298031";
-
 const TABLE_WIDTH = 25;
 const TABLE_LENGTH = 45;
 
+const FRONT_FEET_X = -TABLE_LENGTH + 35;
+const BACK_FEET_X = TABLE_LENGTH - 16;
+const LEFT_FEET_Z = 0;
+const RIGHT_FEET_Z = 15;
+const TABLE_FEET_Y = 0;
+const TABLE_FEET_WIDTH = 2.5;
+const TABLE_FEET_HEIGHT = 15;
+const TABLE_FEET_DEPTH = 3;
+
+const GENERIC_BORDER_WIDTH = 1;
+const GENERIC_BORDER_DEPTH = 1;
+const TABLE_BORDERS_Y = 9;
 const BORDER_HEIGHT = 3;
 
+const TABLE_FEET_COLOR = "#663300";
+const LIGHT_COLOR = "#f5f1ef";
+const TABLE_COLOR = "#298031";
+
+const LIGHT_X_POSITION = 0;
 const LIGHT_Y_POSITION = 30;
+const LIGHT_Z_POSITION = 0;
+
+const LIGHTBULB_RADIUS = 1;
+const LIGHTBULB_SEGMENT_WIDTH_HEIGHT = 32;
 
 //* Initialize webGL with camera and lights
 const canvas = document.getElementById("mycanvas");
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setClearColor('rgb(255,255,255)');
+renderer.shadowMap.enabled = true;
 
 // create scene and camera
 const scene = new THREE.Scene();
@@ -27,19 +39,15 @@ const camera = new THREE.PerspectiveCamera(70, canvas.width / canvas.height, 0.1
 camera.position.set(-50, 25, 20); // camera position
 
 const ambientLight = new THREE.AmbientLight(0x909090);
-// const directionalLight = new THREE.DirectionalLight(0x444444);
-
 const spotLight = new THREE.SpotLight(LIGHT_COLOR);
-renderer.shadowMap.enabled = true;
+
 spotLight.castShadow = true;
+spotLight.position.set(LIGHT_X_POSITION, LIGHT_Y_POSITION, LIGHT_Z_POSITION);
 
-// directionalLight.position.set(0, 10, 0);
-spotLight.position.set(0, LIGHT_Y_POSITION, 0);
-// scene.add(spotLight);
+scene.add(spotLight);
 scene.add(ambientLight);
-// scene.add(directionalLight);
 
-let axesHelper = new THREE.AxesHelper(5);
+const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
 const biliardTable = new THREE.Object3D();
@@ -57,14 +65,16 @@ class Box { //   this class is used for creating the feet and the borders of the
         this.mesh.position.y = yPosition;
         this.mesh.position.z = zPosition;
 
+        this.mesh.castShadow = true;
+        this.mesh.receiveShadow = true;
+
         return this.mesh;
     }
 }
 //  on the helper axis 
-//  z - the blue arrow 
-//  x - the red arrow 
-//  y - the green arrow 
-
+//  z - blue arrow 
+//  x - red arrow 
+//  y - green arrow 
 
 class Sphere { //    this class will be used for the creation of the biliard balls  
     constructor(radius, widthSegment, heightSegment, color, xPosition, yPosition, zPosition) {
@@ -81,31 +91,31 @@ class Sphere { //    this class will be used for the creation of the biliard bal
     }
 }
 
-const lightBulb = new Sphere(1, 32, 32, LIGHT_COLOR, 0, LIGHT_Y_POSITION, 0);
+
+const lightBulb = new Sphere(LIGHTBULB_RADIUS, LIGHTBULB_SEGMENT_WIDTH_HEIGHT, LIGHTBULB_SEGMENT_WIDTH_HEIGHT, LIGHT_COLOR, LIGHT_X_POSITION, LIGHT_Y_POSITION, LIGHT_Z_POSITION);
 
 //  creating the feet of the table
-const leftFrontFoot = new Box(2.5, 15, 3, TABLE_FEET_COLOR, FRONT_FEET_X, TABLE_FEET_Y, LEFT_FEET_Z);
-const rightFrontFoot = new Box(2.5, 15, 3, TABLE_FEET_COLOR, FRONT_FEET_X, TABLE_FEET_Y, RIGHT_FEET_Z);
-const leftBackFoot = new Box(2.5, 15, 3, TABLE_FEET_COLOR, BACK_FEET_X, TABLE_FEET_Y, LEFT_FEET_Z);
-const rightBackFoot = new Box(2.5, 15, 3, TABLE_FEET_COLOR, BACK_FEET_X, TABLE_FEET_Y, RIGHT_FEET_Z);
+const leftFrontFoot = new Box(TABLE_FEET_WIDTH, TABLE_FEET_HEIGHT, TABLE_FEET_DEPTH, TABLE_FEET_COLOR, FRONT_FEET_X, TABLE_FEET_Y, LEFT_FEET_Z);
+const rightFrontFoot = new Box(TABLE_FEET_WIDTH, TABLE_FEET_HEIGHT, TABLE_FEET_DEPTH, TABLE_FEET_COLOR, FRONT_FEET_X, TABLE_FEET_Y, RIGHT_FEET_Z);
+const leftBackFoot = new Box(TABLE_FEET_WIDTH, TABLE_FEET_HEIGHT, TABLE_FEET_DEPTH, TABLE_FEET_COLOR, BACK_FEET_X, TABLE_FEET_Y, LEFT_FEET_Z);
+const rightBackFoot = new Box(TABLE_FEET_WIDTH, TABLE_FEET_HEIGHT, TABLE_FEET_DEPTH, TABLE_FEET_COLOR, BACK_FEET_X, TABLE_FEET_Y, RIGHT_FEET_Z);
 
 //  creating the table top
 const tableTop = new Box(TABLE_LENGTH, 1, TABLE_WIDTH, TABLE_COLOR, 9, 8, 7.5);
 
-const GENERIC_BORDER_WIDTH = 1;
-const GENERIC_BORDER_DEPTH = 1;
-
 //  creating the borders of the table
-const backBorder = new Box(GENERIC_BORDER_WIDTH, BORDER_HEIGHT, TABLE_WIDTH, TABLE_COLOR, 31, 9, 7.5);
-const frontBorder = new Box(GENERIC_BORDER_WIDTH, BORDER_HEIGHT, TABLE_WIDTH, TABLE_COLOR, -13, 9, 7.5);
-const leftBorder = new Box(TABLE_LENGTH, BORDER_HEIGHT, GENERIC_BORDER_DEPTH, TABLE_COLOR, 9, 9, -4.5);
-const rightBorder = new Box(TABLE_LENGTH, BORDER_HEIGHT, GENERIC_BORDER_DEPTH, TABLE_COLOR, 9, 9, 19.5);
+const backBorder = new Box(GENERIC_BORDER_WIDTH, BORDER_HEIGHT, TABLE_WIDTH, TABLE_COLOR, 31, TABLE_BORDERS_Y, 7.5);
+const frontBorder = new Box(GENERIC_BORDER_WIDTH, BORDER_HEIGHT, TABLE_WIDTH, TABLE_COLOR, -13, TABLE_BORDERS_Y, 7.5);
+const leftBorder = new Box(TABLE_LENGTH, BORDER_HEIGHT, GENERIC_BORDER_DEPTH, TABLE_COLOR, 9, TABLE_BORDERS_Y, -4.5);
+const rightBorder = new Box(TABLE_LENGTH, BORDER_HEIGHT, GENERIC_BORDER_DEPTH, TABLE_COLOR, 9, TABLE_BORDERS_Y, 19.5);
+
+const roomFloor = new Box(370, 1, 350, "gray", 0, -8, 0);
 
 //  adding biliard table objects to biliardTable object
 [backBorder, frontBorder, leftBorder, rightBorder, leftBackFoot, leftFrontFoot, rightBackFoot, rightFrontFoot, tableTop].forEach(object => biliardTable.add(object));
 
 //  adding objects to the scene
-[biliardTable, lightBulb, spotLight].forEach(object => scene.add(object));
+[biliardTable, lightBulb, spotLight, roomFloor].forEach(object => scene.add(object));
 
 //* Render loop
 const controls = new THREE.TrackballControls(camera, canvas);
