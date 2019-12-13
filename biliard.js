402ccf1b341556a5f4251d5e7b78671989bdad1e
surgeBinded <ropotdaniel@gmail.com>
2019-12-13 02:23:45 +0100

    added rotation to the balls

--- biliard.js
+++ biliard.js
@@ -1,14 +1,11 @@
-const BALLS_SPEED = 3;
-const FRICTION_AND_REFLECTION = 0.2;
+const BALLS_SPEED = 2;
+const BORDER_TABLE_FRICTION = 0.2;
 const CONTACT_FRICTION = 0.3;
-
-
 const TABLE_TOP_WIDTH = 25;
 const TABLE_TOP_LENGTH = 45;
 const TABLE_X = 9;
 const TABLE_Y = 8;
 const TABLE_Z = 7.5;
-
 const FRONT_FEET_X = -TABLE_TOP_LENGTH + 35;
 const BACK_FEET_X = TABLE_TOP_LENGTH - 16;
 const LEFT_FEET_Z = 0;
@@ -17,27 +14,22 @@ const TABLE_FEET_Y = 0;
 const TABLE_FEET_WIDTH = 2.5;
 const TABLE_FEET_HEIGHT = 15;
 const TABLE_FEET_DEPTH = 3;
-
 const GENERIC_BORDER_WIDTH = 1;
 const GENERIC_BORDER_DEPTH = 1;
 const TABLE_BORDERS_Y = 9;
 const BORDER_HEIGHT = 3;
-
 const TABLE_FEET_COLOR = "#663300";
 const LIGHT_COLOR = "#ffffff";
 const TABLE_COLOR = "#298031";
-
 const LIGHT_X_POSITION = 0;
 const LIGHT_Y_POSITION = 30;
 const LIGHT_Z_POSITION = 0;
-
 const LIGHTBULB_RADIUS = 1;
-const LIGHTBULB_SEGMENT_WIDTH_HEIGHT = 64; // change to 64
-
-const BALLS_RADIUS = 1;
+const LIGHTBULB_SEGMENT_WIDTH_HEIGHT = 64;
+const FACTOR_SPEED = 20;
+const BALLS_RADIUS = 1.1;
 const BALLS_Y_POSITION = 9.7;
 const NUMBER_OF_BALLS = 8;
-
 const ROOM_WIDTH = 370;
 const ROOM_LENGTH = 350;
 const FLOOR_Y_POSITION = -8;
@@ -66,10 +58,8 @@ renderer.shadowMap.enabled = true;
 // create scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(70, canvas.width / canvas.height, 0.1, 1000);
-// camera.position.set(-43, 31, 22); // camera the final view
-// camera.position.set(-50, 25, 20); // camera position
-camera.position.set(-24, 43, 10); // camera view from above
-// camera.position.set(-63, 38, 25);
+camera.position.set(-43, 31, 22); // camera the final view
+// camera.position.set(-24, 43, 10); // camera view from above
 
 const ambientLight = new THREE.AmbientLight(LIGHT_COLOR);
 const spotLight = new THREE.SpotLight(LIGHT_COLOR);
@@ -85,7 +75,6 @@ scene.add(axesHelper);
 
 const biliardTable = new THREE.Object3D();
 biliardTable.position.set(-10, 0, -6);
-// biliardTable.position.set(0, 0, 0);
 
 class Box { //   this class is used for creating the feet and the borders of the table AWA the table plane. 
     constructor(width, height, depth, color, xPosition, yPosition, zPosition) {
@@ -112,22 +101,20 @@ class Box { //   this class is used for creating the feet and the borders of the
         this.meshBBox.setFromObject(this.mesh);
     }
 }
-//  on the helper axis 
-//  z - blue arrow 
-//  x - red arrow 
-//  y - green arrow 
 
 class Sphere { //    this class will be used for the creation of the biliard balls  
     constructor(radius, widthSegment, heightSegment, texture, xPosition, yPosition, zPosition) {
 
+        this.radius = radius;
+
         if (texture === LIGHT_COLOR) {  //  special case for the lightbulb
             this.mesh = new THREE.Mesh(
-                new THREE.SphereGeometry(radius, widthSegment, heightSegment),
+                new THREE.SphereGeometry(this.radius, widthSegment, heightSegment),
                 new THREE.MeshStandardMaterial({ color: LIGHT_COLOR })
             );
         } else {
             this.mesh = new THREE.Mesh(
-                new THREE.SphereGeometry(radius, widthSegment, heightSegment),
+                new THREE.SphereGeometry(this.radius, widthSegment, heightSegment),
                 texture
             );
         }
@@ -142,8 +129,9 @@ class Sphere { //    this class will be used for the creation of the biliard bal
 
         this.mesh.castShadow = true;
         this.mesh.receiveShadow = true;
-    }
 
+        this.mesh.matrixAutoUpdate = false;
+    }
     updateBox() {
         this.meshBoxHelper.setFromObject(this.mesh);
         this.meshBoxHelper.update();
@@ -155,7 +143,7 @@ class Sphere { //    this class will be used for the creation of the biliard bal
 const lightBulb = new Sphere(LIGHTBULB_RADIUS, LIGHTBULB_SEGMENT_WIDTH_HEIGHT, LIGHTBULB_SEGMENT_WIDTH_HEIGHT, LIGHT_COLOR, LIGHT_X_POSITION, LIGHT_Y_POSITION, LIGHT_Z_POSITION);
 lightBulb.mesh.castShadow = false;
 lightBulb.mesh.receiveShadow = false;
-
+lightBulb.mesh.matrix.setPosition(lightBulb.mesh.position);
 
 //  creating the feet of the table
 const leftFrontFoot = new Box(TABLE_FEET_WIDTH, TABLE_FEET_HEIGHT, TABLE_FEET_DEPTH, TABLE_FEET_COLOR, FRONT_FEET_X, TABLE_FEET_Y, LEFT_FEET_Z);
@@ -205,16 +193,15 @@ function plusOrMinus() {
 
 const arrOfVelocities = generateRandomVelocities();
 function generateRandomVelocities() {
-    // const SLOW_DOWN = 2;
+    const SLOW_DOWN = 2;
     const arrOfVelocities = [];
     for (let i = 0; i < NUMBER_OF_BALLS; i++) {
         const { plusOrMinusX, plusOrMinusZ } = plusOrMinus();
-        // arrOfVelocities.push(new THREE.Vector3(BALLS_SPEED * Math.random() * plusOrMinusX, 0, BALLS_SPEED * Math.random() * plusOrMinusZ).divideScalar(SLOW_DOWN));
-        arrOfVelocities.push(new THREE.Vector3(BALLS_SPEED * Math.random() * plusOrMinusX, 0, BALLS_SPEED * Math.random() * plusOrMinusZ));
+        arrOfVelocities.push(new THREE.Vector3(BALLS_SPEED * Math.random() * plusOrMinusX, 0, BALLS_SPEED * Math.random() * plusOrMinusZ).divideScalar(SLOW_DOWN));
+        // arrOfVelocities.push(new THREE.Vector3(BALLS_SPEED * Math.random() * plusOrMinusX, 0, BALLS_SPEED * Math.random() * plusOrMinusZ));
     }
     return arrOfVelocities;
 }
-
 
 function generateBalls() {
     for (let i = 0; i < NUMBER_OF_BALLS; i++) {
@@ -224,7 +211,7 @@ function generateBalls() {
             for (let index = 0; index < arrOfBalls.length; index++) {
 
                 let dist = arrOfBalls[index].mesh.position.clone().sub(biliardBall.mesh.position);
-                //  if the coordinates of the new ball coincide with any of those that are on the table, generate new ones
+                //  if the coordinates of the new ball coincide with any of those that are on the table, generate new coordinates
                 while (dist.length() < 2 * BALLS_RADIUS) {
                     let { randX, randZ } = generateXYCoordinates();
                     biliardBall.mesh.position.x = randX;
@@ -235,12 +222,28 @@ function generateBalls() {
             }
         }
         arrOfBalls.push(biliardBall);
-        arrOfAxes.push(new THREE.Vector3());
+
+        arrOfBalls[i].mesh.matrix.setPosition(arrOfBalls[i].mesh.position);
+        arrOfAxes.push(new THREE.Vector3(0, 1, 0));
         arrOfAxes[i].cross(arrOfVelocities[i]).normalize();
-        arrOfOmega[i]
+        arrOfOmega[i] = arrOfVelocities[i].length() / BALLS_RADIUS;
     }
 }
 generateBalls();
+
+//  adding biliard table objects to biliardTable object
+[backBorder, frontBorder, leftBorder, rightBorder, leftBackFoot, leftFrontFoot, rightBackFoot, rightFrontFoot, tableTop].forEach(object => {
+    biliardTable.add(object.mesh);
+});
+
+//  adding balls to the scene
+arrOfBalls.forEach((ball) => {
+    biliardTable.add(ball.mesh);
+});
+
+//  adding objects to the scene
+[biliardTable, lightBulb.mesh, roomFloor.mesh, roomCeiling.mesh, bulbWire.mesh].forEach(object => scene.add(object));
+
 
 function updateBoxHelpers() {
     [backBorder, frontBorder, leftBorder, rightBorder].forEach(object => {
@@ -253,77 +256,58 @@ function updateBoxHelpers() {
 }
 
 function reflection(dt) {
+
     let index = 0;
     arrOfBalls.forEach(ball => {
+        arrOfVelocities[index].multiplyScalar(1 - BORDER_TABLE_FRICTION * dt);
 
         if (ball.meshBBox.intersectsBox(backBorder.meshBBox)) {
-            arrOfVelocities[index].x = -Math.abs(arrOfVelocities[index].x - arrOfVelocities[index].x * FRICTION_AND_REFLECTION);
+            arrOfVelocities[index].x = -Math.abs(arrOfVelocities[index].x - arrOfVelocities[index].x * BORDER_TABLE_FRICTION);
         }
-
         if (ball.meshBBox.intersectsBox(frontBorder.meshBBox)) {
-            arrOfVelocities[index].x = Math.abs(arrOfVelocities[index].x - arrOfVelocities[index].x * FRICTION_AND_REFLECTION);
+            arrOfVelocities[index].x = Math.abs(arrOfVelocities[index].x - arrOfVelocities[index].x * BORDER_TABLE_FRICTION);
         }
-
         if (ball.meshBBox.intersectsBox(rightBorder.meshBBox)) {
-            arrOfVelocities[index].z = -Math.abs(arrOfVelocities[index].z - arrOfVelocities[index].z * FRICTION_AND_REFLECTION);
+            arrOfVelocities[index].z = -Math.abs(arrOfVelocities[index].z - arrOfVelocities[index].z * BORDER_TABLE_FRICTION);
         }
-
         if (ball.meshBBox.intersectsBox(leftBorder.meshBBox)) {
-            arrOfVelocities[index].z = Math.abs(arrOfVelocities[index].z - arrOfVelocities[index].z * FRICTION_AND_REFLECTION);
+            arrOfVelocities[index].z = Math.abs(arrOfVelocities[index].z - arrOfVelocities[index].z * BORDER_TABLE_FRICTION);
         }
-
-        const dR = new THREE.Matrix4(); // delta-rotational matrix
-        arrOfAxes[index].set(0, 1, 0);
-        arrOfAxes[index].cross(arrOfVelocities[index]).normalize();
-        let ballOmega = arrOfVelocities[index].length() / BALLS_RADIUS;
-        dR.makeRotationAxis(arrOfAxes[index], ballOmega * dt);
-        arrOfBalls[index].mesh.matrix.premultiply(dR);
-
-        //translation along a straight line
-        arrOfBalls[index].mesh.position.add(arrOfVelocities[index].clone().multiplyScalar(dt));
-        arrOfBalls[index].mesh.matrix.setPosition(arrOfBalls[index].mesh.position);
-
-
         index++;
     });
 
-    //now it's "collision of balls against each other"
+    index = 0;
+    arrOfBalls.forEach(ball => {
+        ball.mesh.position.add(arrOfVelocities[index].clone().multiplyScalar(dt));
+        const dR = new THREE.Matrix4();
+        arrOfAxes[index].set(0, 1, 0);
+        arrOfAxes[index].cross(arrOfVelocities[index].clone()).normalize();
+        arrOfOmega[index] = FACTOR_SPEED * arrOfVelocities[index].length() / BALLS_RADIUS;
+        dR.makeRotationAxis(arrOfAxes[index], arrOfOmega[index] * dt);
+        ball.mesh.matrix.premultiply(dR);
+        ball.mesh.matrix.setPosition(ball.mesh.position);
+        ball.updateBox();
+        index++;
+    });
+
     for (let i = 0; i < arrOfBalls.length - 1; i++) {
-        //j tells which ball should i-th ball be compare with (it's about distance between them)
         for (let j = i + 1; j < arrOfBalls.length; j++) {
             if (arrOfBalls[i].mesh.position.distanceTo(arrOfBalls[j].mesh.position) < 2 * BALLS_RADIUS) {
-                let tSpeed = arrOfVelocities[i].clone().multiplyScalar(1 - CONTACT_FRICTION * dt); 	//reducing velocity by 30%
-                arrOfVelocities[i] = arrOfVelocities[j].clone().multiplyScalar(1 - CONTACT_FRICTION * dt);	//reducing velocity by 30%
+                let tSpeed = arrOfVelocities[i].clone().multiplyScalar(1 - CONTACT_FRICTION * dt);
+                arrOfVelocities[i] = arrOfVelocities[j].clone().multiplyScalar(1 - CONTACT_FRICTION * dt);
                 arrOfVelocities[j] = tSpeed;
             }
         }
     }
-
 }
 
 function frictionWithDesk(dt) { //  updates balls speed according to friction
     let index = 0;
     arrOfBalls.forEach(ball => {
-        ball.mesh.position.add(arrOfVelocities[index].multiplyScalar(1 - FRICTION_AND_REFLECTION * dt));
+        ball.mesh.position.add(arrOfVelocities[index].multiplyScalar(1 - BORDER_TABLE_FRICTION * dt));
         index++;
     });
 }
-
-
-//  adding biliard table objects to biliardTable object
-[backBorder, frontBorder, leftBorder, rightBorder, leftBackFoot, leftFrontFoot, rightBackFoot, rightFrontFoot, tableTop].forEach(object => {
-    biliardTable.add(object.mesh);
-});
-
-//  adding balls to the scene
-arrOfBalls.forEach((ball) => {
-    ball.matrixAutoUpdate = false;
-    biliardTable.add(ball.mesh);
-});
-
-//  adding objects to the scene
-[biliardTable, lightBulb.mesh, roomFloor.mesh, roomCeiling.mesh, bulbWire.mesh].forEach(object => scene.add(object));
-
 
 //* Render loop
 const computerClock = new THREE.Clock();
@@ -333,8 +317,8 @@ function render() {
 
     const dt = computerClock.getDelta();
 
-    frictionWithDesk(dt);
     updateBoxHelpers();
+    frictionWithDesk(dt);
     reflection(dt);
 
     controls.update();
